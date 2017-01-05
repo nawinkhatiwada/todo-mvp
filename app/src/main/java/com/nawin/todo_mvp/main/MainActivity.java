d405bfdfbe68d9ba11c7a8b8856c32a6e2c9bf8d
nabin.khatiwada <nabin.khatiwada@braindigit.com>
2017-01-05 14:34:55 +0545

    styles changed

--- app/src/main/java/com/nawin/todo_mvp/main/MainActivity.java
+++ app/src/main/java/com/nawin/todo_mvp/main/MainActivity.java
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import com.nawin.todo_mvp.MVPBus;
@@ -26,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSupportActionBar(mainBinding.toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayShowTitleEnabled(true);
+        }
         requestRecentProductFragment(null);
     }
 
